8e0773ca996632085be3110f8cba759f1f46ef63
AndresMartinez317 <58409933+AndresMartinez317@users.noreply.github.com>
2024-11-08 02:22:34 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
+import java.util.HashSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
+        HashSet<Student> set = new HashSet<Student>();
+        TreeSet<Student> Tset = new TreeSet<>();
+        Stack<Student> stack = new Stack<>();
 
-        System.out.println("Hello world!");
+        Student a = new Student(1,"Andres","Andres@georgian");
+        Student b = new Student(2,"Ivan","Ivan@georgian");
+        Student c = new Student(3,"Martinez","Martinez@georgian");
+        Student d = new Student(2,"test","test@georgian");
+        
+        Tset.add(a);
+        Tset.add(b);
+        Tset.add(c);
+        Tset.add(d);
+
+        for (Student s : Tset) {
+            System.out.println(s.toString());
+        }
+
+
+
+
     }
+
 }
--- Student.java
+++ Student.java
@@ -1,6 +1,63 @@
-public class Student {
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
     int id;
     String name;
     String email;
-    
+
+    public Student(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student student)) return false;
+        return getId() == student.getId() && Objects.equals(getName(), student.getName()) && Objects.equals(getEmail(), student.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getEmail());
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return 0;
+    }
 }
